0f77b72292cd84bbbd0f3590f99d010295a5f39d
Zach Warren <zwarren2@gmail.com>
2016-08-28 16:29:32 -0400

    added hello from railstutorial

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+	def hello
+		render html: "hello, world. Railstutorial is working well so far!"
+	end
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
   Omrails::Application.routes.draw do
-  get "static_pages/home"
+    root 'application#hello'
+    
+    get "static_pages/home"
 
-  get "static_pages/about"
+    get "static_pages/about"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
